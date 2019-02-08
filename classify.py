7919ed9bf500869f1374b2dc8aebabd5ea744fff
deepeshshrestha <47328427+deepeshshrestha@users.noreply.github.com>
2019-02-08 10:37:24 +0545

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 from keras.models import model_from_json
 from keras import backend as K
 import librosa
@@ -8,6 +8,19 @@ import os
 from tkinter import *
 from pygame import mixer
 mixer.init(44100)
+from PIL import Image, ImageTk
+import pandas as pd
+tk=Tk()
+
+canvas_width=500
+canvas_height=300
+
+canvas=Canvas(tk,width=canvas_width,height=canvas_height)
+canvas.pack() 
+
+import matplotlib.pyplot as plt
+import pylab
+
 
 def precision(y_true, y_pred):
     '''Calculates the precision, a metric for multi-label classification of
@@ -108,9 +121,9 @@ def record():
 
 def classify():
     # Example of a Siren spectrogram
-    canvas.delete(result)
     y, sr = librosa.load('recorded.wav',duration=2)
     ps = librosa.feature.melspectrogram(y=y, sr=sr,)
+    px=ps
     ps.shape
     ps= np.array(ps.reshape(1,128,87,1))
     ps.shape
@@ -118,6 +131,30 @@ def classify():
     class_id=(predictions[0])
     print("The recorded chord is "+str(classes[class_id]))
     result=canvas.create_text(800,270,text="The recorded chord is "+str(classes[class_id]),font="Times 15 bold")
+    display(px)
+
+def display(ps):
+    filename = librosa.util.example_audio_file()
+    y, sr = librosa.load('recorded.wav',duration=2)
+    y = y[:100000] # shorten audio a bit for speed
+    window_size = 1024
+    window = np.hanning(window_size)
+    stft  = librosa.core.spectrum.stft(y, n_fft=window_size, hop_length=512, window=window)
+    out = 2 * np.abs(stft) / np.sum(window)
+    # For plotting headlessly
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    fig = plt.Figure()
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+    p = librosa.display.specshow(librosa.amplitude_to_db(out, ref=np.max), ax=ax, y_axis='log', x_axis='time')
+    fig.savefig('test.png',dpi=50)
+    image = Image.open("test.png")
+    photo = ImageTk.PhotoImage(image)
+    label = Label(image=photo)
+    label.image = photo # keep a reference!
+    label.pack()
+
+
 
 def play():
     mixer.Sound("recorded.wav").play()
@@ -137,27 +174,23 @@ loaded_model.compile(
 	loss="categorical_crossentropy",
 	metrics=['accuracy', precision, recall,fmeasure])
           
-tk=Tk()
 
-canvas_width=tk.winfo_screenwidth()
-canvas_height=tk.winfo_screenheight()
-
-canvas=Canvas(tk,width=canvas_width,height=canvas_height)
-canvas.pack() 
 ox,oy=0,0
 
 
 #Buttons
 record=Button(canvas, text='Record',font="Times 15 bold", command=record)
-canvas.create_window(720, 170, window=record, height=25, width=80)
-
-clear=Button(canvas, text='Clear',font="Times 15 bold", command=restart_program)
-canvas.create_window(1000, 570, window=clear, height=25, width=80)
-
-classify=Button(canvas, text='Classify',font="Times 15 bold", command=classify)
-canvas.create_window(880, 170, window=classify, height=25, width=100)
+canvas.create_window(150, 50, window=record, height=25, width=100)
 
 play=Button(canvas, text='Play',font="Times 15 bold", command=play)
-canvas.create_window(800, 170, window=play, height=25, width=80)
+canvas.create_window(250, 50, window=play, height=25, width=50)
+
+classify=Button(canvas, text='Classify',font="Times 15 bold", command=classify)
+canvas.create_window(350, 50, window=classify, height=25, width=100)
+
+clear=Button(canvas, text='Clear',font="Times 15 bold", command=restart_program)
+canvas.create_window(250, 100, window=clear, height=25, width=100)
+
+
 
 mainloop()
