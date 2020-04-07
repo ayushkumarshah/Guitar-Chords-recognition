46e248539db92e6c5c45a783797058a4b9a381e2
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-04-07 14:28:38 +0545

    Add streamlit and imbalanced-learn library

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 conda create -n tf python=3.6
 conda activate tf
 conda install pandas keras=2.2.4
-conda install -c conda-forge librosa
-pip install pygame pillow
-conda install -c anaconda tk
-conda install -c anaconda pyaudio
+conda install -c conda-forge librosa=0.6.1
+pip install pygame==1.9.6 pillow==7.1.1
+conda install -c anaconda tk==8.6.8
+conda install -c anaconda pyaudio==0.2.11
+pip install imbalanced-learn==0.6.2
+pip install streamlit==0.57.3
 conda install -c conda-forge jupyter notebook
